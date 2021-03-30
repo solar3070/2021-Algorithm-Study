3cda78c4edaa172d2c6a83beb1b6fe95af5ff5c8
HyeJun Lee <solar3070@naver.com>
2021-03-30 16:47:06 +0900

    week1

---
+++ 그리디/볼링공 고르기(2).py
@@ -0,0 +1,15 @@
+# 못 푼 문제
+n, m = map(int, input().split())
+weight = list(map(int, input().split()))
+
+# 무게마다 볼링공 개수
+lst = [0] * 11
+for i in weight:
+  lst[i] += 1
+
+result = 0
+for i in range(1, m + 1):
+  n -= lst[i] # 남은 공 개수
+  result += lst[i] * n 
+
+print(result)
