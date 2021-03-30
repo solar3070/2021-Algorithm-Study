02eae15d2dc015c39cd108e373409e791a796e6f
HyeJun Lee <solar3070@naver.com>
2021-03-30 15:41:54 +0900

    week1

--- 그리디/모험가 길드.py
+++ 그리디/모험가 길드.py
@@ -1,7 +1,15 @@
-n = int(input)
+# 못 푼 문제
+n = int(input())
 data = list(map(int, input().split()))
+data.sort()
 
-count = 0
-maxNum = max(data)
+group = 0
+member = 0
 
+for x in data:
+  member += 1
+  if member >= x:
+    group += 1
+    member = 0
 
+print(group)
